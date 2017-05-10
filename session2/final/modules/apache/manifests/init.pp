54cbe7e95fe9fc5fb8c0262d522e90b72e234f42
root <root@localhost.localdomain>
2017-05-10 15:32:01 +0200

    session2

--- session2/final/modules/apache/manifests/init.pp
+++ session2/final/modules/apache/manifests/init.pp
@@ -1,6 +1,17 @@
-class apache{
+class apache {
 
-aaaa
+
+Exec {
+  path => ["/usr/bin", "/bin"],
+}
+package { 'httpd': ensure => installed }
+service { 'httpd': ensure => running }
+file { '/etc/httpd/conf.d/tomcat.conf':
+  ensure => present,
+  source => "/vagrant/puppet-demo/session2/final/httpd_tomcat.conf",
+  require => Package['httpd'],
+  notify => Service['httpd'],
+}
 
 
 }
